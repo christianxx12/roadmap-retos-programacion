8c7450bbf0e55f130e389230b0df4de8d1fb7ca3
Christian Rafael Guado Geronimo <crguado@gmail.com>
2024-07-24 23:30:02 -0500

    #00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/christianxx12.js
@@ -0,0 +1,23 @@
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Comentario de una linea
+
+/*
+Comentario
+de
+varias
+lineas
+*/
+
+let nombre = "christian";
+const PI = 3.14;
+
+let numero = 4
+let string = 'soy un string'
+let boolean1 = true
+let boolean2 = false
+let myUndefined; // Una variable a la que no se le ha asignado un valor tiene el valor undefined
+const bigInt = 12n; // la "n" al final significa que es un BigInt
+let sym2 = Symbol("foo");
+
+console.log('¡Hola, JavaScript!')
